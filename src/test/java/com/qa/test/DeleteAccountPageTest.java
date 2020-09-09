f15f94868550bc15673ad9694858f96ff0fd7bf6
mohitjnj6 <mohitjnj6@gmail.com>
2020-09-10 03:40:35 +0530

    DElete Acc

---
+++ src/test/java/com/qa/test/DeleteAccountPageTest.java
@@ -0,0 +1,67 @@
+package com.qa.test;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.qa.base.TestBase;
+import com.qa.pages.DeleteAccountPage;
+import com.qa.pages.HomePage;
+import com.qa.pages.LoginPage;
+
+public class DeleteAccountPageTest extends TestBase
+{
+	LoginPage loginPage;
+	HomePage homePage;
+	DeleteAccountPage deleteAccountPage;
+	
+	public DeleteAccountPageTest()
+	{
+		super();
+	}
+	
+	@BeforeMethod
+	public void startUp()
+	{
+		initialization();
+	   loginPage = new LoginPage();
+	    homePage = loginPage.verifyLoginBtn(prop.getProperty("username"), prop.getProperty("password"));
+	    homePage.verifydeleteAccountBtn();
+	    deleteAccountPage = new DeleteAccountPage();
+	}
+	
+	@Test
+	public void verifyDeleteAccountPageTitleTest() throws InterruptedException
+	{
+		Thread.sleep(2000);
+		String deleteCustomerTitle = deleteAccountPage.verifyDeleteAccountPageTitle();
+		Assert.assertEquals(deleteCustomerTitle, "Guru99 Bank Delete Account Page");
+		
+	}
+	
+	@Test
+	public void verifySubmitBtnTest() throws InterruptedException
+	{
+		Thread.sleep(2000);
+		String submitMsg = deleteAccountPage.verifySubmitBtn();
+		Assert.assertEquals(submitMsg, "Do you really want to delete this Account?");
+	}
+	
+	
+	@Test
+	public void verifyResetBtnTest()
+	{
+		String submitMsg = deleteAccountPage.verifyResetBtn();
+		Assert.assertEquals(submitMsg, "Do you really want to delete this Account?");
+	}
+	
+		
+	@AfterMethod()
+	public void tearDown()
+	{
+		driver.quit();
+	}
+	
+}
+
